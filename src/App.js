accb62b79afdc8e4dccfcc7bc6e6428524353561
Alexinder Virdee <alex.virdee08@gmail.com>
2021-08-02 21:48:07 -0400

    Added icons. Work on attaching methods & logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import './App.css';
 import Header from './components/Header';
+import Icon from './components/Icon';
 
 function App() {
+  //
+  function addNew() {
+    alert('Add new collection');
+  }
+  //
   return (
     <div className="App">
       <Header />
       <div className="App-main flex flex-col my-8">
-        <div className="m-auto w-2/5">
-        <div className="text-3xl">My Collections</div>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
-          type="text"
-          placeholder="Coins"
-        ></input>
+        <div className="m-auto w-2/5 border border-gray-200 p-8 relative">
+          <div className="text-2xl text-white memento-purple-bg w-3/12 text-center p-2 rounded absolute -top-6 left-10">
+            My Collections
+          </div>
+          <div className="flex flex-row">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-1"
+              type="text"
+              placeholder="Coins"
+            ></input>
+            <Icon className="m-2" icon="trashcan" size={20} color="#504CCF" />
+            <Icon className="m-2" icon="pencil" size={20} color="#504CCF" />
+          </div>
+          <div className="flex flex-row">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-1"
+              type="text"
+              placeholder="World War II Stamps"
+            ></input>
+            <Icon className="m-2" icon="trashcan" size={20} color="#504CCF" />
+            <Icon className="m-2" icon="pencil" size={20} color="#504CCF" />
+          </div>
+          <div className="flex flex-row">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-1"
+              type="text"
+              placeholder="Faberge Eggs"
+            ></input>
+            <Icon className="m-2" icon="trashcan" size={20} color="#504CCF" />
+            <Icon className="m-2" icon="pencil" size={20} color="#504CCF" />
+          </div>
+          <button onClick={addNew} className="memento-purple m-4">
+            + Add New Collection
+          </button>
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Icon from './Icon';
 
 const Header = () => {
     return (
         <div className="main-header flex p-8 justify-around">
             <div className="text-5xl">Memento</div>
-            <button className="header-btn bg-white py-0 px-2 rounded">a-button</button>      
+            <button className="header-btn bg-white py-0 px-2 rounded">
+              <Icon icon="user" size={20} color="#504CCF" />
+              <nbsp className="mr-4"></nbsp>
+              <Icon icon="cog" size={20} color="#504CCF" />
+            </button>      
         </div>
     )
 }
